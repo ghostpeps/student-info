72e1122ebe0edc5e6faa8415d1711b1dbe2f2673
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2025-07-22 21:45:01 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-from home import new_code
+from home import new_code, is_logged_in
 
+if is_logged_in == True:
+  st.title("Settings")
+  st.write(f"Your login code is {new_code}.")
+elif is_logged_in == False:
+  st.title("Please login or sign up first.")
